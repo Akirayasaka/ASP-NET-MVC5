a08b6a232e7d21edabb8c087817a9f4bc81f0fa7
Akira <kuramal12@msn.com>
2020-08-20 17:47:45 +0800

    0820_pr
    Chapter 2

---
+++ 0820/PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using PartyInvites.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            // 透過ViewBag傳遞資料，ViewBag後方的名稱可自定義；等號右邊表示要賦予的資料
+            ViewBag.Greeting = hour < 12 ? "Good Morning!" : "Good Afternoon!";
+            ViewBag.Testing = "Test text";
+            return View();
+        }
+
+        //public string Index()
+        //{
+        //    return "Hello World";
+        //}
+
+        // 預設初始畫面
+        [HttpGet]
+        public ActionResult RsvpForm()
+        {
+            return View();
+        }
+
+        // 接收到POST時
+        [HttpPost]
+        public ActionResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                // 跳到 Thanks.cshtml 並且把表單內強型別資料一起帶入
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                // 參數錯誤
+                return View();
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-ASP-NET-MVC5
+ASP-NET-MVC5 Practice
+【This repository accompanies Pro ASP.NET MVC 5 by Adam Freeman (Apress, 2013).】
+
