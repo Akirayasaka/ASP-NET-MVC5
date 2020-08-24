13bf7a8d098d86ee427dd2ac3d19aa3334d7ece6
Akira <kuramal12@msn.com>
2020-08-24 15:36:17 +0800

    Ch 05
    Razor

---
+++ 0824/Razor/Razor/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Razor.Models
+{
+    public class Product
+    {
+        /// <summary>
+        /// 產品ID
+        /// </summary>
+        public int ProductID { get; set; }
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 描述
+        /// </summary>
+        public string Description { get; set; }
+        /// <summary>
+        /// 價格
+        /// </summary>
+        public decimal Price { get; set; }
+        /// <summary>
+        /// 類別
+        /// </summary>
+        public string Category { get; set; }
+    }
+}
